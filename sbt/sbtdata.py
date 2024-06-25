006c18b9d99fd6fd5cfdeaf3c2e47677767968d5
PhilipByrn3 <typicalphilip@gmail.com>
2024-06-25 16:25:37 -0400

    Update sbtdata.py

--- sbt/sbtdata.py
+++ sbt/sbtdata.py
@@ -2,13 +2,14 @@ import pandas as pd
 import numpy as np
 import math
 
-def ConstructData(timedata, fprdat, fpbdat):
+def ConstructData(timedata, fprdat, fpbdat, measured_velocity):
 
     datapd = {
 
     'Time':timedata,
     'fprdat':fprdat,
     'fpbdat':fpbdat,
+    'measured_velocity':measured_velocity,
 
     }
 
@@ -21,17 +22,16 @@ def ConstructData(timedata, fprdat, fpbdat):
     fprdat_shift = df['fprdat_shift']
     fpbdat_shift = df['fpbdat_shift']
 
-    df.to_csv('sbt_data', encoding="utf-8")
+    measured_velocity = df['measured_velocity']
 
-    return [fprdat, fprdat_shift, fpbdat, fpbdat_shift]
+    df.to_csv('sbt_data.csv', encoding="utf-8")
 
-def findTime(fpdat, fpdat_shift):
+    return [fprdat, fprdat_shift, fpbdat, fpbdat_shift, measured_velocity]
 
-    df = pd.read_csv('sbt_data')
+def findTime(fpdat, fpdat_shift, print_data):
+
+    df = pd.read_csv('sbt_data.csv')
     time_df = df['Time']
-
-    stimelist = []
-    etimelist = []
 
     idx = 1
     j = 0
@@ -41,51 +41,77 @@ def findTime(fpdat, fpdat_shift):
     fpdat_shift = fpdat_shift.copy()
     fpdat = fpdat.copy()
 
-    fpdat_shift[fpdat_shift == 9.81] = 0
+    fpdat_shift[fpdat_shift == 98.10000000000001] = 0
     fpdat_shift[fpdat_shift != 0] = 1
 
-    fpdat[fpdat==9.81] = 0
+    fpdat[fpdat==98.10000000000001] = 0
     fpdat[fpdat!=0] = 1
+    start_times = []  # Stack to keep track of start times
+    pairs = []  # List to store the pairs of start and end times
 
-    for idx in range(len(fpdat_shift)-16):
+    for idx in range(len(fpdat_shift) - 16):
 
         while j < 16:
-            lookback = lookback + fpdat[idx+j]
-            lookahead = lookahead + fpdat_shift[idx+j]
-            j+=1
+            lookback = lookback + fpdat[idx + j]
+            lookahead = lookahead + fpdat_shift[idx + j]
+            j += 1
 
         if fpdat_shift[idx] == 1:
+            if fpdat[idx] == 0 and lookback == 0 and lookahead != 0:
+                start_times.append(time_df[idx])
 
-            if fpdat[idx] == 0 and fpdat_shift[idx+1] == 1 and lookback == 0 and lookahead!=0:
-                print(fpdat_shift[idx], idx,  lookback, lookahead,"START >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", time_df[idx])
-                stimelist.append(time_df[idx])
-                
-
-            elif lookahead-1==0 and lookback!=0:
-                print(fpdat_shift[idx], idx,  lookback, lookahead,"END ==========================", time_df[idx])
-                etimelist.append(time_df[idx])
+            elif lookahead - 1 == 0 and lookback != 0:
+                if start_times:
+                    start_time = start_times.pop()
+                    pairs.append((start_time, time_df[idx]))
+                else:
+                    pass
             else:
-                print(fpdat_shift[idx], idx, lookback, lookahead-1,"######")
                 pass
-
-        else:   
-            print(fpdat_shift[idx], idx, lookback, lookahead-1,"*")
+        else:
             pass
-            
 
         j = 0
         lookback = 0
         lookahead = 0
 
-    # Create pairs of start and end times
+    if print_data is True:
+        for idx in range(len(fpdat_shift) - 16):
+            while j < 16:
+                lookback = lookback + fpdat[idx + j]
+                lookahead = lookahead + fpdat_shift[idx + j]
+                j += 1
 
-    paired_list = list(zip(stimelist, etimelist))
-    total_difference = sum(end - start for start, end in paired_list)
-    tavg = total_difference/len(paired_list)
+            if fpdat_shift[idx] == 1:
+                if fpdat[idx] == 0 and lookback == 0 and lookahead != 0:
+                    print(fpdat_shift[idx], idx, lookback, lookahead, "START >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", time_df[idx])
+                elif lookahead - 1 == 0 and lookback != 0:
+                    if start_times:
+                        print(fpdat_shift[idx], idx, lookback, lookahead, "END ==========================", time_df[idx])
+                    else:
+                        print(fpdat_shift[idx], idx, lookback, lookahead, "UNMATCHED END ==========================", time_df[idx])                 
+                else:
+                    print(fpdat_shift[idx], idx, lookback, lookahead - 1, "######")
+            else:
+                print(fpdat_shift[idx], idx, lookback, lookahead - 1, "*")
+
+            j = 0
+            lookback = 0
+            lookahead = 0
+
+    # Handling any unmatched start times at the end of the loop
+    for unmatched_start in start_times:
+        # print("UNMATCHED START >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", unmatched_start)
+        pass
+
+    total_difference = sum(end - start for start, end in pairs)
+    tavg = total_difference/len(pairs)
     return tavg
 
 
-def AverageSteadyVelocity(slow_time, fast_time, alpha, beta, length, belt_diff):
+def AverageSteadyVelocity(slow_time, fast_time, alpha, beta, length, belt_diff, measured_velocity):
+
+    measured_velocity_avg = measured_velocity.mean()
 
     sinsum = np.sin(alpha) + np.sin(beta)
 
@@ -95,13 +121,14 @@ def AverageSteadyVelocity(slow_time, fast_time, alpha, beta, length, belt_diff):
     timesum = slow_time+fast_time
     sbWalkSpeed = ((2*length*sinsum)-(fast_time*belt_diff))/timesum
     print(
-        "\n Slow Belt Time: ", slow_time, "\n",
+        "========================================\n Slow Belt Time: ", slow_time, "\n",
         "Fast Belt Time: ", fast_time, "\n",
         "Time Sum:", timesum, "\n",
         "Belt Difference: ", belt_diff, "\n",
         "Slow Belt Distance Travelled:", sTravel, "\n",
         "Fast Belt Distance Travelled:", fTravel, "\n",
         "Total Belt Distance Travelled:", sTravel+fTravel, "\n",
-        "Average Steady Velocity: ", sbWalkSpeed, "\n",
+        "Measured Average Steady Velocity: ", measured_velocity_avg, "\n",
+        "Average Steady Velocity: ", sbWalkSpeed, "\n========================================",
         )
 
