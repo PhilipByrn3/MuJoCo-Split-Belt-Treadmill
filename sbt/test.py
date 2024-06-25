626548598fd0ef5ba5c710e8ced930f4e2292e05
PhilipByrn3 <typicalphilip@gmail.com>
2024-06-25 16:25:53 -0400

    Update test.py

--- sbt/test.py
+++ sbt/test.py
@@ -47,10 +47,11 @@ if __name__ == '__main__':
     frames = []
     fprdat_raw = []
     fpbdat_raw = []
+    measured_velocity_data = []
     timedata = []
 
     i = 0
-    belt_diff = 0.5
+    belt_diff = 1.1
 
     while i < 1700:
         sb_physics.step()
@@ -61,20 +62,31 @@ if __name__ == '__main__':
             sb_physics.data.qpos[0] = 0 # Red belt
             sb_physics.data.qpos[1] = 0 # Blue Belt
     
-        fprdat_raw.append(sb_physics.named.data.sensordata[0].copy())
-        fpbdat_raw.append(sb_physics.named.data.sensordata[1].copy())
+        fprdat_raw.append(sb_physics.named.data.sensordata[2].copy())
+        fpbdat_raw.append(sb_physics.named.data.sensordata[5].copy())
+        measured_velocity_data.append(abs(sb_physics.named.data.sensordata['axle_velocimeter'][2].copy()))
         timedata.append(sb_physics.data.time)
 
         img = sb_physics.render(width=640, height=480,camera_id=0)
         frames.append(img)
         i+=1
 
-    datList = ConstructData(timedata, fprdat_raw, fpbdat_raw)
-    fprdat, fprdat_shift, fpbdat, fpbdat_shift = datList
-
-    print(fprdat, fpbdat)
-
-    # time_slow = findTime(fprdat, fprdat_shift)
-    # time_fast = findTime(fpbdat, fpbdat_shift)
-
     render_video(frames)
+
+    datList = ConstructData(timedata, fprdat_raw, fpbdat_raw, measured_velocity_data)
+    fprdat, fprdat_shift, fpbdat, fpbdat_shift, measured_velocity = datList
+
+    print_data = False
+    time_slow = findTime(fprdat, fprdat_shift, print_data)
+    time_fast = findTime(fpbdat, fpbdat_shift, print_data)
+
+    alpha = np.deg2rad(4.5)
+    beta = np.deg2rad(15.5)
+    length = 2
+
+    AverageSteadyVelocity(time_slow, time_fast, alpha, beta, length, belt_diff, measured_velocity)
+
+    
+
+
+# NEED TO ADD DEGREE OF FREEDOM TO WHEEL SO IT CAN MOVE FORWARD
