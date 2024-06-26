5f78769524144bb215d945bdcf5b6aa714338b92
PhilipByrn3 <typicalphilip@gmail.com>
2024-06-26 17:58:41 -0400

    Update test.py

--- sbt/test.py
+++ sbt/test.py
@@ -4,14 +4,20 @@ from dm_control.rl.control import Environment
 from dm_control.suite.base import Task
 from dm_control.mujoco import Physics
 from dm_control import mujoco
+from dm_control.utils import xml_tools
+from dm_control import mjcf
 
 
+import os
+
+from lxml import etree
+import pandas as pd
 import numpy as np
 from moviepy.editor import ImageSequenceClip
 from PIL import Image
 
+import common
 from sbtdata import ConstructData, findTime, AverageSteadyVelocity
-
 
 
 class SbTreadmillTask(Task):
@@ -25,11 +31,18 @@ class SbTreadmillTask(Task):
     def get_reward(self, physics):
         return None
     
-    def action_spec(self, physics):
+    def initialize_episode(self, physics):
         physics.named.data.qvel['redconveyor'] = 1
         physics.named.data.qvel['blueconveyor'] = 1.5
+        physics.named.data.qvel['axleYAxis'] = 0  # Talk about this
 
+def merge_models():
 
+    world = mjcf.RootElement()
+    world.worldbody.add('geom', name='ground',  type='plane', size=[10, 10, 1])
+
+    wheel = mjcf.from_xml_file('wheel.xml')
+    world.attach(wheel)
 
 def render_video(frames):
     clip = ImageSequenceClip(frames, fps=100)
@@ -37,23 +50,13 @@ def render_video(frames):
 
 
 
-if __name__ == '__main__':
-
-    sb_physics = mujoco.Physics.from_xml_path("rimlesstreadmill.xml")
-    sb_task = SbTreadmillTask()
-    sb_env = Environment(physics=sb_physics, task=sb_task)
-    action_spec = sb_env.action_spec()
-
+def simulate_treadmill(belt_diff, i, sb_physics, render_video_enable, print_data):
+    
     frames = []
-    fprdat_raw = []
-    fpbdat_raw = []
-    measured_velocity_data = []
-    timedata = []
-
-    i = 0
-    belt_diff = 1.1
+    fprdat_raw, fpbdat_raw, measured_velocity_data, timedata = [], [], [], []
 
     while i < 1700:
+
         sb_physics.step()
         sb_physics.named.data.qvel['redconveyor'] = 1 # Constant value red (slow) belt
         sb_physics.named.data.qvel['blueconveyor'] = sb_physics.named.data.qvel['redconveyor'] + belt_diff # Blue (fast) belt velocity = Red belt velocity + belt_diff
@@ -66,27 +69,80 @@ if __name__ == '__main__':
         fpbdat_raw.append(sb_physics.named.data.sensordata[5].copy())
         measured_velocity_data.append(abs(sb_physics.named.data.sensordata['axle_velocimeter'][2].copy()))
         timedata.append(sb_physics.data.time)
+        
+        if render_video_enable is True:
+            img = sb_physics.render(width=640, height=480,camera_id=0)
+            frames.append(img)
 
-        img = sb_physics.render(width=640, height=480,camera_id=0)
-        frames.append(img)
         i+=1
-
-    render_video(frames)
+    
+    if render_video_enable is True:
+            
+        render_video(frames)
 
     datList = ConstructData(timedata, fprdat_raw, fpbdat_raw, measured_velocity_data)
-    fprdat, fprdat_shift, fpbdat, fpbdat_shift, measured_velocity = datList
-
-    print_data = False
-    time_slow = findTime(fprdat, fprdat_shift, print_data)
-    time_fast = findTime(fpbdat, fpbdat_shift, print_data)
+    df, fprdat, fprdat_shift, fpbdat, fpbdat_shift, measured_velocity = datList
+    
+    time_slow = findTime(df, fprdat, fprdat_shift, print_data)
+    time_fast = findTime(df, fpbdat, fpbdat_shift, print_data)
 
     alpha = np.deg2rad(4.5)
     beta = np.deg2rad(15.5)
     length = 2
+    
+    measured_velocity_avg, simulated_velocity_avg = AverageSteadyVelocity(time_slow, time_fast, alpha, beta, length, belt_diff, measured_velocity)
 
-    AverageSteadyVelocity(time_slow, time_fast, alpha, beta, length, belt_diff, measured_velocity)
+    return measured_velocity_avg, simulated_velocity_avg
+
+
+
+def loop_simulate_treadmill(belt_diff, i):
+
+    while belt_diff < 1.8:
+
+        measured_velocity_avg, simulated_velocity_avg = simulate_treadmill(belt_diff, i, sb_physics, render_video_enable=False, print_data=False)
+
+        MVAlist.append(measured_velocity_avg)
+        SVAlist.append(simulated_velocity_avg)
+        BDlist.append(belt_diff)
+
+        belt_diff += 0.005
+        sb_env.reset()
+
+    graphpd = {
+
+    'BD': BDlist,
+    'SVA': SVAlist,
+    'MVA': MVAlist,
+
+    }
+    
+    gdf = pd.DataFrame(graphpd)
+
+    gdf.to_csv('Velocities_and BeltDiffs.csv')
+
+
+        
+if __name__ == '__main__':
+
+    merge_models()
+
+    sb_physics = mujoco.Physics.from_xml_path("rimlesstreadmill.xml")
+    sb_task = SbTreadmillTask()
+    sb_env = Environment(physics=sb_physics, task=sb_task)
+    sb_task.initialize_episode(sb_physics)
+
+    MVAlist, SVAlist, BDlist = [], [], []
+
+    render_video_enable = True
+    print_data = False
+
+    belt_diff = 0.5
+    i = 0
+
+    simulate_treadmill(belt_diff, i, sb_physics, render_video_enable, print_data) # Simulates one instance of the treadmill Env
+
+    # loop_simulate_treadmill(belt_diff, i) # Simulates belt_diff/interval instances of treadmill Env
 
     
-
-
-# NEED TO ADD DEGREE OF FREEDOM TO WHEEL SO IT CAN MOVE FORWARD
+    
