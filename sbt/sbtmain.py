6142a03135e1aea2bde9d1f4b885da79a9206447
PhilipByrn3 <typicalphilip@gmail.com>
2024-06-28 15:40:21 -0400

    fixed issue where length value in sbtdata.py did not match the value in wheel.xml, invailidating previous curves found with s vavg. Added commented explanations for functions in sbtmain.py for better readability and ease of maintaining. This version will produce curves very similar to those experimentally found in the Butterfield paper when scaled appropriately, roughly 4x. Note: a difference in this model of the split belt treadmill with a rimless wheel, both belts are moving, as opposed to one belt remaining stationary.

--- sbt/sbtmain.py
+++ sbt/sbtmain.py
@@ -9,14 +9,12 @@ from dm_control import mjcf
 from dm_control.mjcf.base import Element
 
 
-import os
-
 from lxml import etree
 import pandas as pd
 import numpy as np
 from moviepy.editor import ImageSequenceClip
 from PIL import Image
-import glfw
+
 
 import common
 from sbtdata import ConstructData, findTime, AverageSteadyVelocity
@@ -33,10 +31,15 @@ class SbTreadmillTask(Task):
     def get_reward(self, physics):
         return None
     
-    def initialize_episode(self, physics):
-        physics.named.data.qvel['redconveyor'] = 1
-        physics.named.data.qvel['blueconveyor'] = 1.5
-        physics.named.data.qvel['axleYAxis'] = -1  # Talk about this
+    def initialize_episode(self, physics, give_boost=True):
+        # Gives the wheel an initial boost
+        if give_boost is True:
+
+            physics.named.data.qvel['axleYAxis'] = 0
+        
+        else:
+
+            pass
 
 
 def merge_models():
@@ -49,27 +52,28 @@ def merge_models():
     world_mjcf = etree.XML(world_xml_string, parser)
     model_mjcf = etree.XML(model_xml_string, parser)
 
+    # Find XML elements to combine
     world_worldbody = world_mjcf.find('worldbody')
     model_worldbody = model_mjcf.find('worldbody')
 
     world_sensor = world_mjcf.find('sensor')
     model_sensor = model_mjcf.find('sensor')
 
+    # Combine chosen XML elements
     for body in model_worldbody:
         world_worldbody.append(body)
     
     for sensor in model_sensor:
         world_sensor.append(sensor)
 
-    # concat = etree.tostring(world_mjcf, pretty_print=True).decode()
-    # print(concat)
     return etree.tostring(world_mjcf, pretty_print=True).decode()
 
 
 
 def render_video(frames):
+    # Set FPS to frames gathered in simulate_treadmill() accordingly
     clip = ImageSequenceClip(frames, fps=100)
-    clip.write_videofile('my_video.mp4')
+    clip.write_videofile('sbt_video.mp4')
 
 
 
@@ -81,20 +85,25 @@ def simulate_treadmill(belt_diff, i, sb_physics, render_video_enable, print_data
     while i < 2000:
 
         sb_physics.step()
+
+        # Belt velocities are set at the beginning of each step
         sb_physics.named.data.qvel['redconveyor'] = 1 # Constant value red (slow) belt
         sb_physics.named.data.qvel['blueconveyor'] = sb_physics.named.data.qvel['redconveyor'] + belt_diff # Blue (fast) belt velocity = Red belt velocity + belt_diff
 
+        # Belt positions are reset if either of the belts exceed a distance of 10, effectively creating an 'infinite' treadmill
         if (sb_physics.data.qpos[0] > 10) or (sb_physics.data.qpos[1] > 10):
             sb_physics.data.qpos[0] = 0 # Red belt
             sb_physics.data.qpos[1] = 0 # Blue Belt
     
+        # Gather data from sensors and append into list
         fprdat_raw.append(sb_physics.named.data.sensordata[2].copy())
         fpbdat_raw.append(sb_physics.named.data.sensordata[5].copy())
         measured_velocity_data.append(abs(sb_physics.named.data.sensordata['axle_velocimeter'][2].copy()))
         timedata.append(sb_physics.data.time)
         
         if render_video_enable is True:
-            # MUJOCO_GL=egl python test.py # <-----------USE ME TO RUN 
+
+            # MUJOCO_GL=egl python test.py <-----------USE ME TO RUN IN TERMINAL IF RENDERING VIDEO
             img = sb_physics.render(width=640, height=480, camera_id=0)
             frames.append(img)
 
@@ -104,16 +113,20 @@ def simulate_treadmill(belt_diff, i, sb_physics, render_video_enable, print_data
             
         render_video(frames)
 
+    # Create Dataframe for collected data
     datList = ConstructData(timedata, fprdat_raw, fpbdat_raw, measured_velocity_data)
     df, fprdat, fprdat_shift, fpbdat, fpbdat_shift, measured_velocity = datList
     
+    # Calculate the average time of contact for spokes of wheel hitting the sensors on the belts
     time_slow = findTime(df, fprdat, fprdat_shift, print_data)
     time_fast = findTime(df, fpbdat, fpbdat_shift, print_data)
 
+    # Angle offset between spokes of opposite sides and length of spokes
     alpha = np.deg2rad(4.5)
     beta = np.deg2rad(15.5)
-    length = 2
+    length = 2.54 # <-- NEED TO FIX SO THAT VALUE IS NOT CONSTANT, BUT UPDATES WITH MODEL
     
+    # Find velocity using eq from Butterfield paper
     measured_velocity_avg, simulated_velocity_avg = AverageSteadyVelocity(time_slow, time_fast, alpha, beta, length, belt_diff, measured_velocity)
 
     return measured_velocity_avg, simulated_velocity_avg
@@ -122,7 +135,7 @@ def simulate_treadmill(belt_diff, i, sb_physics, render_video_enable, print_data
 
 def loop_simulate_treadmill(belt_diff, i):
 
-    while belt_diff < 4.1:
+    while belt_diff <= 4.55:
 
         measured_velocity_avg, simulated_velocity_avg = simulate_treadmill(belt_diff, i, sb_physics, render_video_enable=False, print_data=False)
 
@@ -148,13 +161,14 @@ def loop_simulate_treadmill(belt_diff, i):
 
         
 if __name__ == '__main__':
-    sb_physics = mujoco.Physics.from_xml_string(merge_models())
 
-    # sb_physics = mujoco.Physics.from_xml_path('rimlesstreadmill.xml')
+    # Initialize Phyics, Task, Env, and initial conditions for SBT and wheel
+    sb_physics = mujoco.Physics.from_xml_string(merge_models())
     sb_task = SbTreadmillTask()
     sb_env = Environment(physics=sb_physics, 
                          task=sb_task)
     
+    # If you want to give your object some initial velocity, set give_boost to true and configure the function
     sb_task.initialize_episode(sb_physics)
 
     MVAlist, SVAlist, BDlist = [], [], []
@@ -162,8 +176,7 @@ if __name__ == '__main__':
     render_video_enable = True
     print_data = False
 
-    #1.3
-    belt_diff = 0.5
+    belt_diff = 0.55
     i = 0
 
     #simulate_treadmill(belt_diff, i, sb_physics, render_video_enable, print_data) # Simulates one instance of the treadmill Env
